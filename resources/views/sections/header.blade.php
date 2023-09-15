7fa722add5da8d9e1ddd243329a1cb980629c659
fabio.lacerra <fabio.lx26@gmail.com>
2023-09-15 17:11:28 +0200

    feat: master: converting original index, footer and header into blades.

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,11 +1,6 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
-
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
+<header class="the-header">
+    @php
+        get_template_part( 'template-parts/header/contacts-social' );
+        get_template_part( 'template-parts/header/navbar' );
+    @endphp
 </header>
